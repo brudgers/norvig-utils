1bdadd0c863c66083f1531818bb9713abacf9b32
ben rudgers <ben.rudgers@gmail.com>
2017-02-27 14:31:23 -0600

    added :norvig-debugger package

---
+++ norvig-debugger.lisp
@@ -0,0 +1,35 @@
+(defpackage :norvig-debugger
+  (:use :common-lisp)
+  (:export :*dbg-ids*
+           :dbg
+           :start-debug
+           :undebug
+           :dbg-indent))
+
+(in-package :norvig-debugger)
+
+(defvar *dbg-ids* nil
+  "Identifiers used by dbg")
+
+(defun dbg (id format-string &rest args)
+  "Print debugging information if (DEBUG-ID) has been specified."
+  (when (member id *dbg-ids*)
+    (fresh-line *debug-io*)
+    (apply #'format *debug-io* format-string args)))
+
+;;; Using start-debugging because 'debug' reserved in SBCL
+(defun start-debug (&rest ids)
+  "Start dbg output on the given ids."
+  (setf *dbg-ids* (union ids *dbg-ids*)))
+
+(defun undebug (&rest ids)
+  "Stop dbg on the ids. With no ids, stop debugging altogether."
+  (setf *dbg-ids* (if (null ids) nil
+                    (set-difference *dbg-ids* ids))))
+
+(defun dbg-indent (id indent format-string &rest args)
+  "Print indented debugging info if (DEBUG ID) has been specified."
+  (when (member id *dbg-ids*)
+    (fresh-line *debug-io*)
+    (dotimes (i indent) (princ "   " *debug-io*))
+    (apply #'format *debug-io* format-string args)))
